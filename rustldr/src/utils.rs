c20ee516ee2d8814f315ca5c944cbb03253486c0
Mark <2578653827@QQ.com>
2023-03-01 21:51:08 +0800

    fix hash_string

--- rustldr/src/utils.rs
+++ rustldr/src/utils.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{Read};
+const HASH_KEY: u32 = 5381;
 
 pub fn load_file_into_memory(path: &str, memory_size: &mut u64) -> *mut u8 {
 
@@ -19,24 +20,19 @@ pub fn load_file_into_memory(path: &str, memory_size: &mut u64) -> *mut u8 {
     image_buffer
 }
 
-pub unsafe fn hash_string(string: &[u8], length: usize) -> u32 {
-    const HASH_KEY: u32 = 0x01000193;
+pub unsafe fn hash_string(string: *mut i8, length: usize) -> u32 {
     let mut hash: u32 = HASH_KEY;
+    let mut ptr: *mut i8 = string;
 
-    for (i, &c) in string.iter().enumerate() {
-        if i == length {
-            break;
-        }
+    for i in 0..length {
+        let c = *ptr.offset(i as isize);
         if c == 0 {
             break;
         }
-        let character = if c >= b'a' {
-            c - 0x20
-        } else {
-            c
-        };
-        hash = (hash << 5) + hash + u32::from(character);
+        let character = if c >= 97 { c - 32 } else { c };
+        hash = hash.wrapping_mul(33).wrapping_add(character as u32);
     }
+    
 
     hash
 }
